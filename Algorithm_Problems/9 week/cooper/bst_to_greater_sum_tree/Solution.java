a950bb339a514eb8d9bbf9aa541608207929c69b
pbg0205 <pbg0205@naver.com>
2021-09-08 16:51:14 +0900

    [#19][leetcode] bst to greater sum tree

---
+++ Algorithm_Problems/9 week/cooper/bst_to_greater_sum_tree/Solution.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    private int val = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            //right
+            bstToGst(root.right);
+
+            //root
+            val += root.val;
+            root.val = val;
+
+            //left
+            bstToGst(root.left);
+        }
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
